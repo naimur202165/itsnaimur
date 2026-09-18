83242776691616f1d98eacd27948800e53063ebb
naimur <naimur202165@gmail.com>
2026-09-18 08:40:34 +0600

    framer work

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -122,7 +122,40 @@ export const allVideoProjects: VideoProject[] = [
 
 // UGC Shorts
 
- 
+//  AI FACELESS 
+  {
+    id: "Ap66hV0qr-U",
+    video_title: "The Kettlebell Prying Squad",
+    video_description: "The kettlebell prying squat is a dynamic mobility and strength exercise where you hold a kettlebell at your chest, sink into the bottom of a deep squat, and use your elbows to gently push your knees apart while shifting side to side to open up tight hips and ankles",
+    tags: ["Short Form", "Social Media", "Dynamic", "Viral"],
+    cover_image: "Ap66hV0qr-U",
+    publish_date: "2026-07-18",
+    client_name: "Liftzip",
+    client_image: "",
+    client_feedback: "",
+    video_link: "https://www.youtube.com/watch?v=Ap66hV0qr-U",
+    project_images: [],
+    category: ["AI Faceless",],
+    duration: "7:45min",
+    software_used: ["Premiere Pro", "After Effects"],
+  },
+  {
+    id: "MnoDI1sdodA",
+    video_title: "Farmers walk kettlebell",
+    video_description: "A kettlebell farmer walk is a simple, powerful full-body exercise where you hold a heavy kettlebell in each hand and walk forward. It builds massive grip strength, locks in your core, and fixes your posture while also improving your cardiovascular fitness. It’s a great exercise for building strength, endurance, and stability.",
+    tags: ["Short Form", "Social Media", "Dynamic", "Viral"],
+    cover_image: "MnoDI1sdodA",
+    publish_date: "2026-07-28",
+    client_name: "Liftzip",
+    client_image: "",
+    client_feedback: "",
+    video_link: "https://www.youtube.com/watch?v=MnoDI1sdodA",
+    project_images: [],
+    category: ["AI Faceless",],
+    duration: "8:49min",
+    software_used: ["Premiere Pro", "After Effects"],
+  },
+//  AI FACELESS 
 
 ];
 
